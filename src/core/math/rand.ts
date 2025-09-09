c08150d78ea91e2a3aa4387407200b42eb66adb9
dj friedman <buildingwithdj+git@gmail.com>
2025-09-09 18:53:42 -0400

    add random utility (automaticaly adds to global stuff)

---
+++ src/core/math/rand.ts
@@ -0,0 +1,27 @@
+const rand = {
+  /**
+   * @param min minium
+   * @param max exclusive maxium
+   * @returns random number
+   */
+  range(min: number = 0, max: number = 1) {
+    return Math.random() * (max - min) - min;
+  },
+  /**
+   * range but ints
+   */
+  int(min: any = 0, max: any = 2) {
+    return Math.floor(rand.range(Math.ceil(min), Math.floor(max)));
+  },
+  /**
+   * int but inclusive
+   */
+  inclusiveint(min: any = 0, max: any = 1) {
+    return Math.floor(rand.range(Math.ceil(min), Math.floor(max) + 1));
+  },
+};
+export default rand;
+Object.defineProperty(Math, "rand", {
+  value: rand,
+  writable: false,
+});
