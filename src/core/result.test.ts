0a2db0546558f0721668c2624766c2d5acb2c475
dj friedman <buildingwithdj+git@gmail.com>
2025-10-15 13:59:11 -0400

    fix using new instead of the premade functions

--- src/core/result.test.ts
+++ src/core/result.test.ts
@@ -1,16 +1,17 @@
 import { describe, expect, expectTypeOf, test } from "vitest";
-import { Err, Ok } from "./result";
+import { err, ok } from "./result";
+import { ResultAsync } from "./result-async";
 
 describe("test both result types", () => {
   test("check Ok type", () => {
-    const current = new Ok("cheese");
+    const current = ok("cheese");
     expect(current).toHaveProperty("value");
     expect(current.unwrap()).toBe("cheese");
     expect(current.value).toBe("cheese");
     expect(current).not.toHaveProperty("error");
   });
   test("check Err type", () => {
-    const current = new Err("cheese");
+    const current = err("cheese");
     expect(current).toHaveProperty("error");
     expect(current).not.toHaveProperty("value");
     expect(current.unwrapErr()).toBe("cheese");
